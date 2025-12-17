a0d3c594ca84f87cd5a8b496f9ba378b99ac21b6
DmitryKarp4 <karpyukd090607@gmail.com>
2025-12-17 21:15:24 +0500

    Добавил /techinfo и некоторые QOL решения

--- tgbot.py
+++ tgbot.py
@@ -2,6 +2,7 @@ import telebot
 from config import TG_TOKEN
 from checkpassword import check_password
 from checkip import is_valid_ip
+from techinfo import techinfo
 
 bot = telebot.TeleBot(TG_TOKEN)
 
@@ -20,6 +21,7 @@ def send_help(message):
         "/help - Список доступных команд\n"
         "/ip <IP-адрес> - Проверка валидности IP-адреса\n"
         "/checkpassword <Пароль> - Проверка надежности пароля\n"
+        "/techinfo <IP-адрес> - Техническая информация по IP-адресу через Shodan и Censys\n"
     )
     bot.reply_to(message, help_text)
 
@@ -48,4 +50,24 @@ def check_password_handler(message): # Обработчик команды /chec
     is_strong, feedback = check_password(password, user_id) # Получает две переменные, is_strong и feedback, из функции check_password, одна из которых указывает, надежен ли пароль, а другая содержит сообщение для пользователя
     bot.reply_to(message, feedback) # Ответ бота с соответствующим сообщением
 
+# Обработчик команды /techinfo -> техническая информация по IP-адресу
+@bot.message_handler(commands=['techinfo'])
+def tech_info_handler(message):
+    parted_message = message.text.split(maxsplit=1)
+    if len(parted_message) != 2:
+        bot.reply_to(message, "Пожалуйста, используйте команду в формате: /techinfo <IP-адрес>")
+        return
+    is_valid, feedback = is_valid_ip(parted_message[1], message.from_user.id)
+    if is_valid:
+        ip = parted_message[1]
+        bot.reply_to(message, f"Получение технической информации для IP-адреса {ip}...")
+    else:
+        bot.reply_to(message, "Пожалуйста, введите действительный IP-адрес.")
+        return
+    tech_info_result = techinfo(ip, message.from_user.id) # Заменить на tech_info когда добавится Censys
+    bot.reply_to(message, tech_info_result)
+
 bot.infinity_polling()
+
+
+# TODO: Shodan Dork search
