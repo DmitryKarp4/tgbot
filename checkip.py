abdf5f96cfa91e5502bd66c92067ca62ce0dc65b
DmitryKarp4 <karpyukd090607@gmail.com>
2025-12-22 01:49:05 +0500

    Добавил обратное разрешение IP-адресов

--- checkip.py
+++ checkip.py
@@ -5,7 +5,12 @@ def is_valid_ip(ip_str, user_id) -> tuple[bool, str, str|None]:
     try:
             ipaddress.ip_address(ip_str)
             log_event(f"Проверка IP-адреса {ip_str} от пользователя {user_id} завершена: действительный IP-адрес.")
-            return True, "Это действительный IP-адрес."
+            try:
+                hostname = socket.gethostbyaddr(ip_str)
+                log_event(f"Обратное разрешение IP-адреса {ip_str} от пользователя {user_id} успешно.")
+            except:
+                log_event(f"Обратное разрешение IP-адреса {ip_str} от пользователя {user_id} не удалось.")
+            return True, f"Это действительный IP-адрес, имя хоста {hostname[0] if hostname else 'не найдено'}", ip_str
     except ValueError:
             log_event(f"Проверка IP-адреса {ip_str} от пользователя {user_id} завершена: недействительный IP-адрес.")
             try:
